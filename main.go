6cc2e18104398e2b0b596eaea48f3e3819e7b254
David Ochepo <ochepodayvid@gmail.com>
2026-04-30 13:27:52 +0100

    initial commit - clean setup

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age int
+	fmt.Println("My initial age is", age)
+	age = 20
+	fmt.Println("My age after first assignment is", age)
+	age = 30
+	fmt.Println("My age after second assignment is", age)
+}
